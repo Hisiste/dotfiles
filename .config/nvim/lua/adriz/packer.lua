18b2b7b6e53cf818fde4b201183106740b468608
Adrián Ramírez <aditooliva@gmail.com>
2024-03-02 10:31:05 -0600

    [Nvim] Was using an old release of telescope this whole time. Omg.

--- .config/nvim/lua/adriz/packer.lua
+++ .config/nvim/lua/adriz/packer.lua
@@ -25,8 +25,8 @@ return require('packer').startup(function(use)
 
     -- Telescope
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '*',
+        -- or                          , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
